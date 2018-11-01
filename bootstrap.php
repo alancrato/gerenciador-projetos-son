48a23c5534e1c09fb16da6b0baf9d08a6e046e87
Alan Rodrigues <alancrato@hotmail.com>
2018-11-01 10:01:21 -0300

    init project php

---
+++ bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+use SON\Framework\Router;
+
+require __DIR__.'/vendor/autoload.php';
+
+new Router;
---
+++ src/Router.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SON\Framework;
+
+class Router
+{
+    /**
+     * Router constructor.
+     */
+    public function __construct()
+    {
+        echo '<h1>Hello Word!</h1>';
+    }
+}
