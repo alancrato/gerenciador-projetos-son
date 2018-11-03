7816be2d08a263b516dcf0b6059eb689ff328375
Alan Rodrigues <alancrato@hotmail.com>
2018-11-02 23:45:56 -0300

    init model

--- config/routes.php
+++ config/routes.php
@@ -1,15 +1,21 @@
 <?php
 
+use App\Model\User;
+
 $router->add('GET','/', function () use ($container){
-    $pdo = $container['db'];
-    var_dump($pdo);
-    return 'Home page';
+    $user = new User($container);
+    $data = $user->get(1);
+
+    var_dump($data);
 });
 
 $router->add('GET','/projects', function (){
     return 'Projects page';
 });
 
-$router->add('GET','/projects/(\d+)', function ($params){
-    return 'Project page id:' . $params[1];
+$router->add('GET','/users/(\d+)', function ($params) use ($container){
+    $user = new User($container);
+    $data = $user->get($params[1]);
+
+    return "My name is: " . $data['name'];
 });
